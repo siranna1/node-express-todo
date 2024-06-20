fbb1f937d54993fc7bc7e2261e53547200f7036b
Takehito Fukuyori <60061139+siranna1@users.noreply.github.com>
2024-06-20 07:14:27 +0000

    13回の変更

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const pg = require('pg');
 
-let todos = [];
+const connection = new pg.Pool({
+  host: 'localhost',
+  user: 'root',
+  password: 'postgres',
+  database: 'todo_app',
+  port: 5432,
+  });
 
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'ToDo App',
-    todos: todos,
-  });
+  connection.query(
+    `select * from tasks;`,
+    (error, results) => {
+      console.log(error);
+      console.log(results);
+      res.render('index', {
+        title: 'ToDo App',
+        todos: results,
+      });
+    }
+  );
 });
 
 router.post('/', function (req, res, next) {
+  connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return
+    }
+    console.log('success');
+  });
+
   const todo = req.body.add;
-  todos.push(todo);
-  res.redirect('/');
+  onnection.query(
+    `insert into tasks (user_id, content) values (1, '${todo}');`,
+    (error, results) => {
+      console.log(error);
+      res.redirect('/');
+    }
+  );
+  
+  
 });
 
 module.exports = router;
